8ddec5bd9ad51e1b021a8f44ed169ef655b0791a
Ilya Kanyshev <26299517+we1come32@users.noreply.github.com>
2023-10-26 21:03:22 +0300

    [A] Adding models

---
+++ models.py
@@ -0,0 +1,87 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy import String, Integer, Boolean
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
+    blocked: Mapped[bool] = mapped_column(Boolean())
+
+    subscriptions: Mapped["Subscribe"] = relationship(back_populates="addresses")
+
+    def __repr__(self):
+        return f"<User id={self.id}>"
+
+
+class Services(Base):
+    """
+    Таблица с разрешёнными сервисами
+    """
+    __tablename__ = "services"
+
+    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
+    name: Mapped[str] = mapped_column(String(20))
+    base_url: Mapped[str] = mapped_column(String())
+    activated: Mapped[bool] = mapped_column(Boolean(), default=False)
+
+    def __repr__(self):
+        return f"<Service id={self.id} name={self.name} url={self.base_url}>"
+
+
+class Subscription(Base):
+    """
+    RSS-ссылка на подписку о новостях
+    """
+    __tablename__ = "subscriptions"
+
+    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
+    url: Mapped[str] = mapped_column(String(length=150))
+
+    service_id: Mapped[int] = mapped_column(ForeignKey("services.id"))
+
+
+class Subscribe(Base):
+    """
+    Подписка пользователей на определённые RSS-ссылки
+    """
+    __tablename__ = "user_subscriptions"
+
+    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
+
+    subscription_id: Mapped[int] = mapped_column(ForeignKey("subscriptions.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+
+class Task(Base):
+    """
+    Задачи с сервисов
+    """
+    __tablename__ = "tasks"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    service_id: Mapped[int] = mapped_column(ForeignKey("services.id"))
+    title: Mapped[str] = mapped_column(String(50))
+    description: Mapped[str] = mapped_column(String(600))
+    url: Mapped[str] = mapped_column(String(50))
+    themes: Mapped[str] = mapped_column(String(50))
+    price: Mapped[str] = mapped_column(String(50))
+    limit_time: Mapped[str] = mapped_column(String(50))
+    publish_date: Mapped[str] = mapped_column(String(50))
+    author: Mapped[str] = mapped_column(String(50))
+
+
+class TaskViewStatus(Base):
+    __tablename__ = "task_views"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    task_id: Mapped[int] = mapped_column(ForeignKey("tasks.id"))
