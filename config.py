7377c3682025f13829c95d2260584776a502246e
Ilya Kanyshev <26299517+we1come32@users.noreply.github.com>
2023-11-13 21:41:54 +0300

    [E] Moving messages to another file

--- config.py
+++ config.py
@@ -5,31 +5,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Messages
-new_task_message = """<b>Новая задача! Платформа <i>{platform}</i></b>
-
-<b>Название</b>: <i>{title}</i>
-<b>Описание</b>: <i>{description}</i>
-<b>Сроки</b>: <i>{limit_time}</i>
-<b>Цена</b>: <i>{price}</i>
-
-<b>Темы</b>: <i>{themes}</i>
-
-{published_date}. Автор: {author}
-"""
-
-short_new_task_message = """<b>Новая задача! Платформа <i>{platform}</i></b>
-
-<b>Название</b>: <i>{title}</i>
-<b>Описание</b> по ссылке ниже <i>(short msg)</i>
-<b>Сроки</b>: <i>{limit_time}</i>
-<b>Цена</b>: <i>{price}</i>
-
-<b>Темы</b>: <i>{themes}</i>
-
-{published_date}. Автор: {author}
-"""
-
 
 # Database section
 
---
+++ messages.py
@@ -0,0 +1,19 @@
+new_task_message = """<b>Новая задача! Платформа <i>{platform}</i></b>
+
+<b>Название</b>: <i>{title}</i>
+<b>Описание</b>: <i>{description}</i>
+
+<b>Темы</b>: <i>{themes}</i>
+
+{published_date}
+"""
+
+short_new_task_message = """<b>Новая задача! Платформа <i>{platform}</i></b>
+
+<b>Название</b>: <i>{title}</i>
+<b>Описание</b> по ссылке ниже <i>(short msg)</i>
+
+<b>Темы</b>: <i>{themes}</i>
+
+{published_date}
+"""
