96dc141cc68849f68d06b51c6597b000597a4c62
Ilya Kanyshev <26299517+we1come32@users.noreply.github.com>
2023-11-13 22:41:04 +0300

    [A] Added adding to db subscription

--- utils/chat_worker.py
+++ utils/chat_worker.py
@@ -1,18 +1,26 @@
+from typing import Any, Union, Dict
 from urllib.parse import urlsplit
 
-from aiogram import Dispatcher
-from aiogram.filters import CommandStart, Command
+from aiogram import Dispatcher, F
+from aiogram.filters import CommandStart, Command, BaseFilter
 from aiogram.types import Message
 from loguru import logger
 from sqlalchemy import insert, select
 from sqlalchemy.orm import Session
 
-from models import engine, Service, User
+from config import TG_ADMIN_ID
+from models import engine, Service, User, Subscription, Subscribe
+
+
+class DevBotFilter(BaseFilter):
+    async def __call__(self, *args: Any, **kwargs: Any) -> Union[bool, Dict[str, Any]]:
+        pass
+
 
 dp = Dispatcher()
 
 
-@dp.message(CommandStart())
+@dp.message(DevBotFilter(), CommandStart())
 async def command_start_handler(message: Message) -> None:
     if not message.from_user:
         return
@@ -23,7 +31,7 @@ async def command_start_handler(message: Message) -> None:
     await message.answer(f"Привет!")
 
 
-@dp.message(Command(commands=["add_rss", "add"]))
+@dp.message(DevBotFilter(), Command(commands=["add_rss", "add"]))
 async def add_rss(message: Message):
     msg_text = message.text.split()
     result_message = "Please, send rss-link with this command."
@@ -36,9 +44,31 @@ async def add_rss(message: Message):
             if url.netloc and (services := ss.execute(rq).fetchall()):
                 service: Service = services[0][0]
                 if service.activated:
-                    result_message = f"Hello, its working! This service is {service.name!r}"
+
+                    # Get or create url subscription
+                    rq = select(Subscription).where(Subscription.url == url.geturl())
+                    if (subs := ss.execute(rq).fetchall()):
+                        sub = subs[0][0]
+                    else:
+                        # Creating
+                        ss.execute(insert(Subscription).values(url=url.geturl(), service_id=service.id))
+                        # Get sub.id from db
+                        sub = ss.execute(select(Subscription).where(Subscription.url == url.geturl())).fetchall()[0][0]
+                    logger.debug(sub)
+
+                    # Get or create user subscription
+                    rq = select(Subscribe).where(Subscribe.subscription_id == sub.id)
+                    if not(ss.execute(rq).fetchall()):
+                        ss.execute(insert(Subscribe).values(subscription_id=sub.id, user_id=message.from_user.id))
+                    ss.commit()
+                    result_message = f"Hello, its working! You subscribe onto {url.geturl()!r}."
                 else:
                     result_message = f"Service from url {service.base_url} is not allowed."
             else:
                 result_message = "Unknown service or service is not allowed."
     await message.reply(result_message)
+
+
+@dp.message(Command(commands=["allow"]), F.from_user.id.in_([TG_ADMIN_ID]))
+async def allow_service(message: Message):
+    pass
